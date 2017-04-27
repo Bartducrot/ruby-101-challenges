191e7758fea5b0ff47de9832cb5921078bf8c605
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 16:30:00 +0100

    done

--- 04-Acronymize/lib/acronymize.rb
+++ 04-Acronymize/lib/acronymize.rb
@@ -1,4 +1,6 @@
 def acronymize(sentence)
   # TODO: return the acronym on the given sentence
-
+  res = ""
+  (sentence.split(" ").map! { |e| e[0].upcase }).each { |letter| res += letter}
+  return res
 end
