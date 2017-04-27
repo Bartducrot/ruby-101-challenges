bfa808f070d1851d70cd597b9b125dcbd0a4c743
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 16:35:47 +0100

    done

--- 05-Pythagorean-triples/lib/pythagorean_triple.rb
+++ 05-Pythagorean-triples/lib/pythagorean_triple.rb
@@ -1,4 +1,8 @@
 def pythagorean_triple?(a, b, c)
   # TODO: return true/false depending if numbers a, b and c are Pythagorean triples
-
+  if (a.is_a? Integer) && (b.is_a? Integer) && (c.is_a? Integer)
+    a*a + b*b == c*c
+  else
+    false
+  end
 end
