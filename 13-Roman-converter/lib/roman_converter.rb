869cddf2d720cdeaec890f9e5d0a9f0389516f1f
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 19:24:10 +0100

    done

--- 13-Roman-converter/lib/roman_converter.rb
+++ 13-Roman-converter/lib/roman_converter.rb
@@ -1,4 +1,16 @@
+ROMAN_TO_INT = {"M" => 1000, "C" => 100, "L" => 50, "X" => 10 , "V" => 5, "I" => 1}
 def roman_converter(roman_string)
   # TODO: return the Roman numeral string's corresponding integer
-
+  arr = (roman_string.split("").map { |letter| ROMAN_TO_INT[letter] })
+  res = 0
+  arr.each_with_index do |number, index|
+    if arr[index + 1].nil? || arr[index] >= arr[index + 1]
+      res += number
+    else
+      res -= number
+    end
+  end
+  res
 end
+
+p roman_converter("MMCMLXXXIII")
