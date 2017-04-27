38c85451c743a6716926aadc942cb9d132137554
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 16:51:34 +0100

    done

--- 07-Word-frequency/lib/word_frequency.rb
+++ 07-Word-frequency/lib/word_frequency.rb
@@ -1,4 +1,10 @@
 def word_frequency(sentence)
   # TODO: return a hash representing the number of apperances of words in the sentence
-
+  sentence = sentence.delete(",;:!?./§")
+  arr = sentence.split(" ")
+  res = Hash.new {0}
+  arr.each do |word|
+    res[word.downcase] += 1
+  end
+  res
 end
