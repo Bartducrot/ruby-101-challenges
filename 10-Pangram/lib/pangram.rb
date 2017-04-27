c6f71b5de3104dcb2a701a807614b344738f260b
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 17:35:09 +0100

    done

--- 10-Pangram/lib/pangram.rb
+++ 10-Pangram/lib/pangram.rb
@@ -1,4 +1,4 @@
 def pangram?(sentence)
   # TODO: return true/false depending if the sentence is a pangram (contains every letter of the alphabet)
-
+  "azertyuiopqsdfghjklmwxcvbn".split("").sort == sentence.delete(" ?./§,;:!-").delete(" ?./§,;:!-").downcase.split("").sort.uniq
 end
