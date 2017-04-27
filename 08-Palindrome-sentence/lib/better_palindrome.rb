68f120a4ba9ce3e44874c5009830f52b9ea41ac3
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 17:07:21 +0100

    done

--- 08-Palindrome-sentence/lib/better_palindrome.rb
+++ 08-Palindrome-sentence/lib/better_palindrome.rb
@@ -1,4 +1,5 @@
 def better_palindrome?(sentence)
   # TODO: return true/false depending if the *sentence* is a palindrome
-
+  # sentence = sentence.delete(" ?./§,;:!")
+  sentence.delete(" -?./§,;:!").downcase == sentence.delete(" -?./§,;:!").downcase.reverse
 end
