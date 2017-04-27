c5695f9b4acfb5f4a50c1012981c690c97bc8931
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 16:46:30 +0100

    done

--- 06-FizzBuzz/lib/fizzbuzz.rb
+++ 06-FizzBuzz/lib/fizzbuzz.rb
@@ -1,4 +1,17 @@
 def fizzbuzz(max)
   # TODO: return an Array
-
+  # return every number until max, replacing number % 3 = 0 by "Fizz" /// and number % 5 == 0 by "Buzz"
+  res = []
+  (1..max).to_a.each do |number|
+    if number % 15 == 0
+      res << "FizzBuzz"
+    elsif number % 3 == 0
+      res << "Fizz"
+    elsif number % 5 == 0
+      res << "Buzz"
+    else
+      res << number
+    end
+  end
+  return res
 end
