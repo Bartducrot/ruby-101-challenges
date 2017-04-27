b28cee898d2c144f7383157bd632128bfb98070b
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-04-27 17:20:40 +0100

    done

--- 09-DNA-transcription/lib/transcription.rb
+++ 09-DNA-transcription/lib/transcription.rb
@@ -1,4 +1,16 @@
 def dna_transcription(dna_sequence)
   # TODO: return the RNA complement of the incoming DNA sequence
-
+  dna_sequence = (dna_sequence.delete_if { |letter| !(['C', 'G', 'T', 'A'].include? letter.upcase)}).map { |letter| letter.upcase }
+  dna_sequence.map! do |letter|
+    if letter == "C"
+      letter = "G"
+    elsif letter == "G"
+      letter = "C"
+    elsif letter == "T"
+      letter = "A"
+    else
+      letter = "U"
+    end
+  end
+  dna_sequence
 end
